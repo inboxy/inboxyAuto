d0168d78c552d08b579a0c2c1ae618838e0d6c18
Inboxy <46248658+inboxy@users.noreply.github.com>
2025-09-02 21:21:53 +0100

    Update script.js
    indexdb

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+//indexDB
+const DB_NAME = 'SensorDB';
+const DB_VERSION = 1;
+const STORE_NAME = 'readings';
+
+function openDB() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+
+    request.onupgradeneeded = event => {
+      const db = event.target.result;
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, { keyPath: 'timestamp' });
+      }
+    };
+
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // script.js
 
 let recording = false;
